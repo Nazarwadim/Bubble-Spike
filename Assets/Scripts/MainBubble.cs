bc02e9e36ce7f295d1965d6594b418855dc28a50
Kharachko <volodymyr.kharachko.pp.2022@lpnu.ua>
2025-01-24 21:52:52 +0200

    Add wood spawn

---
+++ Assets/Scripts/MainBubble.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class MainBubble : MonoBehaviour, IDamageable
+{
+    [SerializeField] int health;
+    const int MaxDamage = 10000000;
+
+    private bool _isDead = false;
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            ActionBus.MainBubbleKilled?.Invoke();
+            print("Died");
+            _isDead = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent(out IKillable component))
+        {
+            TakeDamage(component.GetDamage());
+
+            if (!_isDead && other.TryGetComponent(out IDamageable damageable))
+            {
+                damageable.TakeDamage(MaxDamage);
+            }
+
+        }
+    }
+}
