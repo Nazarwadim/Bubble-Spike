b8c47b454238174c917d4f354ad8a53978608dd1
Kharachko <volodymyr.kharachko.pp.2022@lpnu.ua>
2025-01-29 22:12:37 +0200

    BetterBarAnimation

--- Assets/Scripts/ProgressBarView.cs
+++ Assets/Scripts/ProgressBarView.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,8 +6,12 @@ public class ProgressBarView : MonoBehaviour
 {
     [SerializeField] private Image _fillImage;
     [SerializeField] private float _max = 1;
+    [SerializeField] private float _smoothTime = 0.7f;
 
     [SerializeField] private ScoreProgress _score;
+
+    private float _targetFillAmount;
+    private Coroutine _smoothFillCoroutine;
 
     private void OnEnable()
     {
@@ -20,8 +25,27 @@ public class ProgressBarView : MonoBehaviour
 
     public void SetProgress(int progress)
     {
-        float value = progress / _max;
-        value = Mathf.Clamp01(value);
-        _fillImage.fillAmount = value;
+        _targetFillAmount = Mathf.Clamp01(progress / _max);
+
+        if (_smoothFillCoroutine == null)
+        {
+            _smoothFillCoroutine = StartCoroutine(SmoothFill());
+        }
+    }
+
+    private IEnumerator SmoothFill()
+    {
+        while (true)
+        {
+            _fillImage.fillAmount = Mathf.Lerp(_fillImage.fillAmount, _targetFillAmount, Time.deltaTime / _smoothTime);
+
+            if (Mathf.Abs(_fillImage.fillAmount - _targetFillAmount) < 0.01f)
+            {
+                _fillImage.fillAmount = _targetFillAmount;
+                _smoothFillCoroutine = null;
+                yield break;
+            }
+            yield return null;
+        }
     }
 }
